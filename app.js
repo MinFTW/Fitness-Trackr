d7ab9632dc1aa35b5e7a07e87091f94f4c0f5b7e
MinFTW <kimmins2@outlook.com>
2022-07-09 12:49:04 -0500

    created build folder

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const client = require('./db/client');
 const app = express();
+const path = require('path');
 
 require('dotenv').config();
 
@@ -15,5 +16,10 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use('/api', require('./api'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
